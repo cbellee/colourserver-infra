f7d725dce20d691fe501d215d2e37496ac82bd22
Chris Bellee <8634922+cbellee@users.noreply.github.com>
2020-03-02 22:39:14 +1100

    enabled adv. networking for AKS - fixed .tf v2

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -43,7 +43,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "aks-subnet-0"
   resource_group_name  = azurerm_resource_group.rg.name
   address_prefix       = "10.0.1.0/24"
-  virtual_network_name = azurerm_virtual_network.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
 }
 
 resource "azurerm_kubernetes_cluster" "k8s" {
